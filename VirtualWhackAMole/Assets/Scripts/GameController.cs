db01fbbbb4e8953731403fef5cd0ef144972e2ca
Mitchell Hermon <mhermon05@gmail.com>
2021-02-27 02:40:18 -0600

    Organize codebase and add ability to "hit" moles by clicking them.

---
+++ VirtualWhackAMole/Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    /*Game lasts 30 seconds, first mole shows after 1 second.
+    * Mole is shown for 2 seconds, there is a .8 second break, and another
+    * mole appears. 
+    */
+    private float gameTimer = 30f;
+    private float showMoleTimer = 1f;
+    private float hideMoleTimer;
+    private Mole[] moles;
+    private Mole targetMole;
+    private bool showingMole = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // adding Moles into list 
+        moles = GameObject.FindObjectsOfType<Mole>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTimer -= Time.deltaTime; 
+        if (gameTimer > 0f)
+        {
+            showMoleTimer -= Time.deltaTime;
+            hideMoleTimer -= Time.deltaTime;
+        
+            if (showMoleTimer < 0f && showingMole == false)
+            {
+                targetMole = moles[Random.Range(0, moles.Length)];
+                targetMole.ShowMole();
+                showingMole = true;
+                hideMoleTimer = 2f;
+            }
+            if (hideMoleTimer < 0f && showingMole == true)
+            {
+                targetMole.HideMole();
+                showMoleTimer = .8f;
+                showingMole = false;
+            }
+        }
+    }
+
+}
---
+++ VirtualWhackAMole/Assets/Scripts/Mole.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour
+{
+    private float speed; 
+    private Vector3 endPosition; 
+    private Vector3 inPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set both endPosition and inPosition to the starting position
+        // to prevent movement at start
+        inPosition = transform.localPosition;
+        endPosition = inPosition;
+        speed = 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, endPosition,
+            Time.deltaTime * speed);
+    }
+
+    // Retract the mole back towards the start position
+    public void HideMole()
+    {
+        endPosition = inPosition;
+    }
+
+    // Start mole movement towards new endPosition of showing
+    public void ShowMole()
+    {
+        endPosition.x += 1.5f;
+    }
+
+    // On mouse click stop movement and return to start position
+    private void OnMouseDown()
+    {
+        endPosition = inPosition;
+        transform.localPosition = endPosition;
+    }
+
+}
