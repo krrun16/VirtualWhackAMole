16b1f51df297a6e99b4bf5e4ef474dedbb2783a9
Hovland <hvland@uiowa.edu>
2021-06-17 14:24:59 -0500

    Current progress on screen reader.
    Can navigate  main menu with arrow keys. Sound plays when button selected. Can click options button with enter.

--- VirtualWhackAMole/Assets/Scenes/KinectView/Scripts/OptionsMenu.cs
+++ VirtualWhackAMole/Assets/Scenes/KinectView/Scripts/OptionsMenu.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using UnityEngine.EventSystems;
 
 public class OptionsMenu : MonoBehaviour
 {
 
     private ToggleGroup hintGroup;
     private ToggleGroup handGroup;
+    public AudioSource Declarative;
+    public AudioSource Imperative;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +19,29 @@ public class OptionsMenu : MonoBehaviour
         handGroup = GameObject.Find("ToggleHandGroup").GetComponent<ToggleGroup>();
     }
 
+    private void Awake()
+    {
+        Declarative.Play();
+    }
+    private void Update()
+    {
+       // if keydown , change hint, play hint change.
+       if (Input.GetKeyDown("down"))
+        {
+            HintChange();
+            
+        }
+    }
     public void HintChange()
     {
-        PlayerPrefs.SetString("HintType", hintGroup.ActiveToggles().First().name); 
+        PlayerPrefs.SetString("HintType", hintGroup.ActiveToggles().First().name);
+        
     }
 
     public void HandChange()
     {
         PlayerPrefs.SetString("DominantHand", handGroup.ActiveToggles().First().name);
     }
+    
+    
 }
